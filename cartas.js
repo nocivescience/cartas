275fd04ad7f1555103f4ce8585151c1cdddecb37
noceive <nocivescience@gmail.com>
2022-06-09 05:19:32 -0400

    termine los card-value que me faltaban

--- cartas.js
+++ cartas.js
@@ -1,6 +1,6 @@
 class AudioControles{
     constructor(){
-        this.backgroundMusic = new Audio("asserts/halloween.mp3");
+        this.backgroundMusic = new Audio("https://raw.githubusercontent.com/WebDevSimplified/Mix-Or-Match/master/Assets/Audio/flip.wav");
         this.backgroundMusic.loop = true;
         this.backgroundMusic.volume = 0.5;
     }
@@ -23,6 +23,19 @@ class MixOrMatch{
             alert("Bienvenido a la Batalla de las Cartas");
         },1000);
     }
+    shuffleCards(cardsArray){
+        for(let i = card.length - 1; i > 0; i--){
+            let randomIndex = Math.floor(Math.random() * (i + 1));
+            [card[i], card[randomIndex]] = [card[randomIndex], card[i]];
+        }
+        cardsArray = cardsArray.map((card,index)=>{
+            card.style.order = index;
+        })
+    };
+    getCardType(card){
+        return card.getElementsByClassName('card-value')[0].id;
+    };
+    
 }
 function update(){
     let overlay = Array.from(document.querySelectorAll(".overlay-text"));
@@ -30,4 +43,8 @@ function update(){
     let game=new MixOrMatch(20,cards);
     game.startGame();
 }
-update();
+if(document.readyState==="loading"){
+    document.addEventListener("DOMContentLoaded",update);
+}else{
+    update();
+}
