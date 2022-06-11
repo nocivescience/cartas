6b3ed3cdc49cfc390940e8bd4cd29ce5449110f1
noceive <nocivescience@gmail.com>
2022-06-11 03:05:56 -0400

    desde aca el emparejamiento y termino

--- cartas.js
+++ cartas.js
@@ -2,21 +2,49 @@ const overlaiesEl=document.querySelectorAll('.overlay-text');
 const gameContainerEl=document.querySelector('.game-container');
 const cardContanerEl=document.querySelector('.card-container');
 const cardsEl=Array.from(document.getElementsByClassName('card'));
+const timeRemainingEl=document.getElementById('time-remaining');
+const bodyEl=document.querySelector('body');
+const flipsEl=document.getElementById('flips');
 class MixOrMatch{
-    constructor(cards=null){
+    constructor(cards,TotalTime=100){
         this.cardsArray=cards;
+        this.TotalTime=TotalTime;
+        this.totalClicks=10;
     }
-    shuffleCards(cardsArray){
-        for(let i=cardsArray.length-1;i>0;i--){
+    shuffleCards(){
+        for(let i=this.cardsArray.length-1;i>0;i--){
             const randomIndex=Math.floor(Math.random()*(i+1));
-            [cardsArray[i],cardsArray[randomIndex]]=[cardsArray[randomIndex],cardsArray[i]];
+            [this.cardsArray[i],this.cardsArray[randomIndex]]=[this.cardsArray[randomIndex],this.cardsArray[i]];
         }
-        cardsArray=cardsArray.map((card,index)=>{
+        this.cardsArray=this.cardsArray.map((card,index)=>{
             card.style.order=index;
         })
     }
-    
+    startCountDown(){
+        setInterval(()=>{
+            this.TotalTime--;
+            timeRemainingEl.textContent=this.TotalTime;
+            if(this.TotalTime<0){
+                timeRemainingEl.textContent='0';
+            }
+        },100);
+    }
+    flipCard(card){
+        this.totalClicks--;
+        flipsEl.textContent=this.totalClicks;
+        if(this.totalClicks<=0){
+            alert('You lost');
+            this.totalClicks=0;
+            flipsEl.textContent=this.totalClicks;
+        }
+    }
 }
-const game=new MixOrMatch();
-game.shuffleCards(cardsEl);
-game.opacityDown();
+const game=new MixOrMatch(cardsEl);
+game.shuffleCards();
+game.startCountDown();
+flipsEl.textContent=game.totalClicks;
+cardsEl.forEach(card=>{
+    card.addEventListener('click',()=>{
+        game.flipCard(card);
+    })
+})
