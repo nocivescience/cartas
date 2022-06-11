964bb7bdc47d9f88e9677935355b1d55f9300cd1
noceive <nocivescience@gmail.com>
2022-06-10 22:39:43 -0400

    ya las cartas están en orden

--- cartas.js
+++ cartas.js
@@ -1,12 +1,22 @@
 const overlaiesEl=document.querySelectorAll('.overlay-text');
 const gameContainerEl=document.querySelector('.game-container');
 const cardContanerEl=document.querySelector('.card-container');
+const cardsEl=Array.from(document.getElementsByClassName('card'));
 class MixOrMatch{
-    constructor(cards){
+    constructor(cards=null){
         this.cardsArray=cards;
     }
-
-}
-document.addEventListener('click', function() {
+    shuffleCards(cardsArray){
+        for(let i=cardsArray.length-1;i>0;i--){
+            const randomIndex=Math.floor(Math.random()*(i+1));
+            [cardsArray[i],cardsArray[randomIndex]]=[cardsArray[randomIndex],cardsArray[i]];
+        }
+        cardsArray=cardsArray.map((card,index)=>{
+            card.style.order=index;
+        })
+    }
     
-});
+}
+const game=new MixOrMatch();
+game.shuffleCards(cardsEl);
+game.opacityDown();
