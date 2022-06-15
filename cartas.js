5e10c759cd718c0ca0ff382e6650dbeff6e1e2d7
noceive <nocivescience@gmail.com>
2022-06-14 21:13:50 -0400

    modificando el css desde aca se recupera el css

--- cartas.js
+++ cartas.js
@@ -1,67 +1,29 @@
 class MixOrMatch{
-    constructor(totalTime, cards) {
-        this.cardsArray = cards;
-        this.totalTime = totalTime;
+    constructor(cards) {
+        this.cards=cards;
+        this.cardsArray=[];
+        this.busy=false;
+        this.emptyCards=null;
     }
-    startGame() {
-        this.totalClicks = 0;
-        this.timeRemaining = this.totalTime;
-        this.cardToCheck = null;
-        this.matchedCards = [];
-        this.busy = true;
-        setTimeout(() => {
-            this.busy = false;        
-            this.shuffleCards(this.cardsArray);
-        }, 1000);
+    PairOfCardsFailed(card1, card2) {
+        this.cardsArray.push(card1);
+        this.cardsArray.push(card2);
+        card1.classList.add('hidden');
     }
-    shuffleCards(cardsArray) {
-        for(let i=0; i<this.cardsArray.length-1; i--) {
-            let randomIndex=Math.floor(Math.random()*(i+1));
-            [cardsArray[i], cardsArray[randomIndex]] = [cardsArray[randomIndex], cardsArray[i]];
-        };
-        cardsArray=cardsArray.map((card,index) => {
-            card.style.order = index;
-        })
-    };
-    flipCard(card) {
-        this.checkForCardMatch(card);
+    showingCards(card){
+        
     }
-    checkForCardMatch(card) {
-        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
-            this.cardMatch(card, this.cardToCheck);
-        else 
-            this.cardMismatch(card, this.cardToCheck);
-
-        this.cardToCheck = null;
-    };
-    cardMatch(card1, card2) {
-        this.matchedCards.push(card1);
-        this.matchedCards.push(card2);
-        if(this.matchedCards.length===this.cardsArray.length){
-            console.log('victory');
-        }
-    };
-    cardMismatch(card1, card2) {
-        this.busy = true;
-        setTimeout(() => {
-            this.busy = false;
-            console.log(card1, card2);
-        });
-    };
-    getCardType(card) {
-        return card.getElementsByClassName('card-value')[0].id;
-    };
-    canFlipCard(card) {
-        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    checkCardType(card){
+        return card.querySelectorAll('.card-value')[0].id;
     };
 }
-function update(){
-    const cards=Array.from(document.querySelectorAll('.card'));
-    const gamesEl= new MixOrMatch(10, cards);
+function ready() {
+    const cards = Array.from(document.querySelectorAll('.card'));
+    const mixOrMatch = new MixOrMatch(cards);
     cards.forEach(card => {
-        card.addEventListener('click', event => {
-            gamesEl.flipCard(card);
+        card.addEventListener('click', () => {
+            mixOrMatch.showingCards(card);
         });
     });
-}
-update();
+};
+ready();
