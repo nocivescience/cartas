9e16a6995565fcc82eba0d544348861603b5da65
noceive <nocivescience@gmail.com>
2022-06-17 03:05:23 -0400

    con boton the this bussy pero no funciona tampoco

--- cartas.js
+++ cartas.js
@@ -1,29 +1,54 @@
 class MixOrMatch{
-    constructor(cards) {
-        this.cards=cards;
-        this.cardsArray=[];
-        this.busy=false;
-        this.emptyCards=null;
+    constructor(totalTime, cards) {
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
     }
-    PairOfCardsFailed(card1, card2) {
-        this.cardsArray.push(card1);
-        this.cardsArray.push(card2);
-        card1.classList.add('hidden');
+    startGame() {
+        this.cardToCheck = null;
+        this.matchedCards = [];
+        this.busy = true;
+        setTimeout(() => {
+            this.busy = false;
+        }, 500)
+    };
+    flipCard(card) {
+        if (this.canFlipCard(card)) {
+            if(this.cardToCheck) {
+                this.checkForCardMatch(card);
+            }else{
+                this.cardToCheck = card;
+            }
+        }
+    };
+    checkForCardMatch(card){
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck)) {
+            this.checkForCardMatch(card, this.cardToCheck);
+        }
+        this.cardToCheck = null;
+    };
+    checkForCardMatch(card1,card2){
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('active');
+        card2.classList.add('active');
     }
-    showingCards(card){
-        
-    }
-    checkCardType(card){
-        return card.querySelectorAll('.card-value')[0].id;
+    canFlipCard(card) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    };
+    cardType(card) {
+        return card.getElementsByClassName('card-value')[0].id;
     };
 }
-function ready() {
-    const cards = Array.from(document.querySelectorAll('.card'));
-    const mixOrMatch = new MixOrMatch(cards);
+function ready(){
+    let cards=Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(60,cards);
+    document.getElementById('laCosaNostra').addEventListener('click',()=>{
+        game.startGame();
+    });
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            mixOrMatch.showingCards(card);
-        });
+            game.flipCard(card);
+        })
     });
-};
+}
 ready();
