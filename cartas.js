c35b554558adc412b6fe28a986768b7398858dd0
noceive <nocivescience@gmail.com>
2022-06-18 21:49:15 -0400

    esto lo hice yo y quedo mal de nuevo

--- cartas.js
+++ cartas.js
@@ -12,11 +12,10 @@ class MixOrMatch{
         this.cardToCheck = null;
         this.matchedCards = [];
         this.busy = true;
-        setTimeout(() => {
-            this.shuffleCards(this.cardsArray);
+
             this.countdown = this.startCountdown();
             this.busy = false;
-        }, 500)
+     
         this.hideCards();
         this.timer.innerText = this.timeRemaining;
         this.ticker.innerText = this.totalClicks;
@@ -25,17 +24,10 @@ class MixOrMatch{
         return setInterval(() => {
             this.timeRemaining--;
             this.timer.innerText = this.timeRemaining;
-            if(this.timeRemaining === 0)
-                this.gameOver();
-        }, 1000);
-    };
-    gameOver() {
-        clearInterval(this.countdown);
-        document.getElementById('game-over-text').classList.add('visible');
-    };
-    victory() {
-        clearInterval(this.countdown);
-        document.getElementById('victory-text').classList.add('visible');
+            document.getElementById(
+                'game-over-text'
+            ).classList.add('visible');
+        },1000);
     };
     hideCards() {
         this.cardsArray.forEach(card => {
@@ -43,77 +35,20 @@ class MixOrMatch{
             card.classList.remove('matched');
         });
     };
-    flipCard(card) {
-        if(this.canFlipCard(card)) {
-            this.totalClicks++;
-            this.ticker.innerText = this.totalClicks;
-            card.classList.add('visible');
-            if(this.cardToCheck) {
-                this.checkForCardMatch(card);
-            } else {
-                this.cardToCheck = card;
-            }
-        }
-    };
-    checkForCardMatch(card) {
-        if(this.getCardType(card) === this.getCardType(this.cardToCheck)) {
-            this.cardMatch(card,this.cardToCheck)
-        } else {
-            this.cardMismatch(card,this.cardToCheck)
-        }
-        this.cardToCheck = null;
-    };
-    cardMatch(card1, card2) {
-        this.matchedCards.push(card1);
-        this.matchedCards.push(card2);
-        card1.classList.add('matched');
-        card2.classList.add('matched');
-        if(this.matchedCards.length === this.cardsArray.length) {
-            this.victory();
-        }
-    };
-    cardMismatch(card1, card2) {
-        this.busy = true;
-        setTimeout(() => {
-            card1.classList.remove('visible');
-            card2.classList.remove('visible');
-            this.busy = false;
-        }, 1000);
-    };
-    getCardType(card) {
-        return card.getElementsByClassName('card-value')[0].id;
-    };
-    shuffleCards(cardsArray) {
-        for(let i = cardsArray.length - 1; i > 0; i--) {
-            let randomIndex = Math.floor(Math.random() * (i + 1));
-            [cardsArray[i], cardsArray[randomIndex]] = [cardsArray[randomIndex], cardsArray[i]];
-        }
-        cardsArray=cardsArray.map((card,index) => {
-            card.style.order = index;
-        });
-    };
-    canFlipCard(card) {
-        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
-    };
 }
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready);
-}else {
-    ready();
-}
+// if(document.readyState === 'loading') {
+//     document.addEventListener('DOMContentLoaded', ready);
+// }else {
+//     ready();
+// };
 function ready() {
-    let cards = Array.from(document.getElementsByClassName('card'));
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let game = new MixOrMatch(60, cards);
-    overlays.forEach(overlay => {
-        overlay.addEventListener('click', () => {
+    const overlays=document.querySelectorAll('.overlay-text');
+    const gamesEl=new MixOrMatch(30,overlays);
+    overlays.forEach(overlay=>{
+        overlay.addEventListener('click',function(){
             overlay.classList.remove('visible');
-            game.startGame();
-        });
-    });
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            game.flipCard(card);
-        });
+            gamesEl.startGame();
+        })
     });
 }
+ready();
