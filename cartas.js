3f96d919724a7da77965b0a8cd64f4b6a9d31a7f
noceive <nocivescience@gmail.com>
2022-06-19 17:42:22 -0400

    finalizada la v.3

--- cartas.js
+++ cartas.js
@@ -16,44 +16,81 @@ class MixOrMatch{
             this.shuffleCards(this.cardsArray);
             this.busy = false;
         }, 500)
+        this.hideCards();
         this.timer.innerText = this.timeRemaining;
         this.ticker.innerText = this.totalClicks;
     }
+    startCountdown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0) {
+                this.gameOver();
+            }
+        },1000);
+    };
     shuffleCards(cardsArray) {
         for(let i = cardsArray.length - 1; i > 0; i--) {
             let randomIndex = Math.floor(Math.random() * (i + 1));
-            let itemAtIndex = cardsArray[randomIndex];
-            cardsArray[randomIndex] = cardsArray[i];
-            cardsArray[i] = itemAtIndex;
+            [cardsArray[i], cardsArray[randomIndex]] = [cardsArray[randomIndex], cardsArray[i]];
         }
+        cardsArray=cardsArray.map((card, index) => {
+            card.style.order = index;
+        });
     };
     flipCard(card) {
-        card.classList.add('visible');
-        this.totalClicks++;
-        this.ticker.innerText = this.totalClicks;
-        if(this.cardToCheck) {
-            this.checkForCardMatch(card);
-        } else {
-            this.cardToCheck = card;
+        if(this.carFlipCard(card)) {
+            card.classList.add('visible');
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+            if(this.cardToCheck) {
+                this.checkForCardMatch(card);
+            } else {
+                this.cardToCheck = card;
+            };
         };
     }
+    carFlipCard(card) {
+        return !this.busy && !this.matchedCards.includes(card) && !card.classList.contains('visible');
+    };
+    gameOver() {
+        clearInterval(this.countdown);
+        document.getElementById('game-over-message').classList.add('visible');
+    };
     checkForCardMatch(card) {
         if(this.getTypeCard(card) === this.getTypeCard(this.cardToCheck)) {
-            alert('match');
+            this.cardMatch(card, this.cardToCheck);
         }else{
-            alert('no match');
+            this.cardMismatch(card, this.cardToCheck);
         }
         this.cardToCheck = null;
     };
+    victory(){
+        clearInterval(this.countdown);
+    }
     cardMatch(card1,card2){
         this.matchedCards.push(card1);
         this.matchedCards.push(card2);
         card1.classList.add('matched');
         card2.classList.add('matched');
         if(this.matchedCards.length === this.cardsArray.length) {
-            alert('You found them all!');
+            this.victory();
         }
     }
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        });
+    };
+    cardMismatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible'); //this busy es para que no se pueda hacer click en las cartas de nuevo
+            card2.classList.remove('visible');
+            this.busy = false;
+        });
+    };
     getTypeCard(card) {
         return card.getElementsByClassName('card-value')[0].id;
     };
