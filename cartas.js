0b7664ca1c373fd1f56d7e3888baf4b58048b4fa
noceive <nocivescience@gmail.com>
2022-06-21 19:54:58 -0400

    version 5 en desarrollo 1

--- cartas.js
+++ cartas.js
@@ -3,17 +3,60 @@ class MixOrMatch{
         this.totalTime=totalTime;
         this.cards=cards;
         this.busy=true;
+        this.timer=document.getElementById('time-remaining');
+        this.ticker=document.getElementById('flips')
+        this.matched=false;
+        this.totalFlips=0;
+        this.matchedCards=[];
+        this.cardToCheck=null;
+    }
+    hideCards(){
+        this.cards.forEach((card)=>{
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        })
+    }
+    checkCardMatch(card){
+        console.log(this.getCardType(card))
+    }
+    getCardType(card){
+        return card.getElementsByClassName('card-value')[0].id;
+    }
+    runningGame(){
+        this.startCountDown()
     }
     startCountDown(){
         return setInterval(()=>{
-            
-        })
+            console.log(this.totalTime--)
+            this.timer.innerText=this.totalTime;
+        },1000)
+    }
+    flipCard(card){
+        if(this.canFlipCard(card)){
+            card.classList.add('visible')
+            this.totalFlips++
+            this.ticker.textContent=this.totalFlips;
+        }
+    };
+    canFlipCard(card){
+        return this.busy
     }
 }
 function ready(){
-    const cards=Array.from(document.getElementsByClassName('.card'))
-    const overlays=Array.from(document.getElementsByClassName('.overlay-text'))
+    const cards=Array.from(document.getElementsByClassName('card'))
+    const overlays=Array.from(document.getElementsByClassName('overlay-text'))
     const games=new MixOrMatch(100,cards)
-    games.startGame()
+    overlays.forEach((overlay)=>{
+        overlay.addEventListener('click',()=>{
+            overlay.classList.remove('visible');
+            games.runningGame();
+        })
+    })
+    cards.forEach(card=>{
+        card.addEventListener('click',()=>{
+            games.flipCard(card)
+            games.checkCardMatch(card)
+        })
+    })
 }
 ready();
